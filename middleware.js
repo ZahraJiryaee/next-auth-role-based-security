4ecdb1769d3531bc66f6089651e771debb67f7b4
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2024-04-01 17:38:09 +0330

    only admin role can visit CreateUser

--- middleware.js
+++ middleware.js
@@ -1,5 +1,29 @@
-export { default } from "next-auth/middleware";
+import { withAuth } from "next-auth/middleware";
+import { NextResponse } from "next/server";
+
+// export { default } from "next-auth/middleware";
+// protect everything
+
+export default withAuth(
+  function middleware(req) {
+    console.log("withAuth pathname", req.nextUrl.pathname);
+    console.log("withAuth role", req.nextauth);
+
+    if (
+      req.nextUrl.pathname.startsWith("/CreateUser") &&
+      req.nextauth.token?.role !== "admin"
+    ) {
+      return NextResponse.rewrite(new URL("/Denied", req.url));
+    }
+  },
+  {
+    callbacks: {
+      authorized: ({ token }) => !!token,
+    },
+  }
+);
 
 export const config = {
   matcher: ["/CreateUser"],
 };
+// protect CreateUser in middleware
