d5b970708f787bb429b71dd3427c926a14612d3f
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2024-04-01 16:16:47 +0330

    implement logina and logout with google and github providers

---
+++ app/api/auth/[...nextauth]/options.js
@@ -0,0 +1,50 @@
+// setup how you want ppl to authenticate with the app
+import GitHubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+
+export const options = {
+  providers: [
+    GitHubProvider({
+      profile(profile) {
+        console.log("Profile of GitHub:", profile);
+
+        let userRole = "GitHub User";
+        if (profile?.email == "zahrajiryaee77@gmail.com") {
+          userRole = "admin";
+        }
+
+        return {
+          ...profile,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+    GoogleProvider({
+      profile(profile) {
+        console.log("Profile of Google:", profile);
+
+        return {
+          ...profile,
+          id: profile.sub,
+          role: userRole,
+        };
+      },
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) token.role = user.role;
+      return token;
+      // add the role to the token so we can use it on the server side
+    },
+    async session({ session, token }) {
+      if (session?.user) session.user.role = token.role;
+      return session;
+      // client ide usage
+    },
+  },
+};
