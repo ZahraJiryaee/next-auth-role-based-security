0b5034d12b834bfa8ef49d13d1f864f06a83726b
Zahra Jiryaee <zahrajiryaee77@gmail.com>
2024-04-01 19:28:28 +0330

    implement login with credentials with next-auth

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -1,6 +1,9 @@
 // setup how you want ppl to authenticate with the app
 import GitHubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import User from "@/app/(models)/User";
+import bcrypt from "bcrypt";
 
 export const options = {
   providers: [
@@ -35,6 +38,46 @@ export const options = {
       },
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET,
+    }),
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: {
+          label: "email",
+          type: "text",
+          placeholder: "your-email",
+        },
+        password: {
+          label: "password",
+          type: "password",
+          placeholder: "your-password",
+        },
+      },
+      async authorize(credentials) {
+        try {
+          const foundUser = await User.findOne({ email: credentials.email })
+            .lean()
+            .exec();
+
+          if (foundUser) {
+            const match = await bcrypt.compare(
+              credentials.password,
+              foundUser.password
+            );
+
+            if (match) {
+              delete foundUser.password;
+
+              foundUser["role"] = "Unverified Email";
+
+              return foundUser;
+            }
+          }
+        } catch (error) {
+          console.log("credentials login error", error);
+        }
+        return null; // not-authenticated
+      },
     }),
   ],
   callbacks: {
